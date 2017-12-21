63b65a8850536d4c0cca77fab095247115ffd31d
Brendan Koral <brendanhkoral@gmail.com>
2017-12-20 20:31:25 -0600

    Add queries for author, post, and posts

---
+++ src/schema/queries/post.js
@@ -0,0 +1,16 @@
+import { GraphQLString, GraphQLNonNull, GraphQLInt } from "graphql"
+import { Post } from "../types/Post"
+import { fakeDatabase } from "../FakeDatabase";
+
+export default {
+    post: {
+        type: Post,
+        description: "Get details about a specific post",
+        args: {
+            id: { type: new GraphQLNonNull(GraphQLInt) }
+        },
+        resolve: function(parent, {id}) {
+            return fakeDatabase.getBlogPost(id)
+        }
+    }
+}
